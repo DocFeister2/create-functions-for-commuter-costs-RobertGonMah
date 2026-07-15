d8f186646f63f18f58f058596ba86340adeae6bf
RobertGonMah <robertgmah@gmail.com>
2026-07-15 22:17:34 +0000

    0715_1517: Added the missing lines and adjusted.

--- main.cpp
+++ main.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 const double DOLLARS_PER_GALLON = 4.49;     // cost of gas per gallon
 const double MILES_PER_COMMUTE = 20.5;    // miles in one round-trip commute
 const double MILES_PER_GALLON = 36;       // car fuel efficiency
 
-double myCommutes;
+int myCommutes;
 double myCost;
 
 // Converts gas dollars into number of commutes
 double toCommutes(double gasDollars) {
-  double gasGallons;
-  double gasMiles;
-  double numCommutes;
-
-  gasGallons = gasDollars / DOLLARS_PER_GALLON;
-  gasMiles = gasGallons * MILES_PER_GALLON;
-
-  numCommutes = gasMiles; // TODO: That math is not right! Something is missing here...
+  double gasGallons = gasDollars / DOLLARS_PER_GALLON; // Decided to declare and initialize these on one line for less code.
+  double gasMiles = gasGallons * MILES_PER_GALLON;
+  double numCommutes = gasMiles / MILES_PER_COMMUTE;
 
   return numCommutes;
 }
 
-// TODO!
-// Converts number of commutes into gas cost
-/*
-double toDollars(double commutes) {
-  ....
-  return totalCost;
+// // Converts number of commutes into gas cost
+double toDollars(int numOfCommutes) {
+  double commuteCost = ((MILES_PER_COMMUTE / MILES_PER_GALLON) * DOLLARS_PER_GALLON); // Decided to declare and initialize this on one line for less code.
+  commuteCost = commuteCost * numOfCommutes;
+  
+  return commuteCost;
 }
-*/
 
 int main() {
+
+  // Not too sure if this was needed but the extra numbers seemed irrelevalant for this application.
+  cout << fixed << setprecision(2);
 
   cout << "$20 will buy you: " << toCommutes(20) << " commutes." << endl;
   cout << "$100.50 will buy you: " << toCommutes(100.50) << " commutes." << endl;
   cout << "$500 will buy you: " << toCommutes(500) << " commutes." << endl;
   cout << "$8000 will buy you: " << toCommutes(8000) << " commutes." << endl;
 
-  cout << "Enter the number of commutes you'd like to do:" << endl;
+  cout << "Enter the number of commutes you'd like to do: ";
   cin >> myCommutes;
 
   myCost = toDollars(myCommutes);
